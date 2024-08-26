e9c01ef959c4dc2dde7bc3ffcbf789bf104d1538
RoyX2009 <royx2027@student.cis.edu.hk>
2024-08-26 09:06:51 +0800

    Update Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        int pagestoread = 100 - age;
+        System.out.println(age + "-year olds should read " + pagestoread + " pages before giving up.");
 
     }
 }
